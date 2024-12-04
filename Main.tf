589389d05e7f5ec79e4809ebecc36914b0d33d97
Tigers83 <Reddvisionart@gmail.com>
2024-12-03 20:07:54 -0500

    Add files via upload

---
+++ Main.tf
@@ -0,0 +1,28 @@
+module "arm-cali" {
+    source = "./arm-cali/class5"
+}
+
+module "hknew" {
+    source = "./hknew/class5"
+}
+
+module "arm-japan" {
+    source = "./arm-japan/class5"
+}
+
+module "arm-ld" {
+    source = "./arm-ld/class5"
+}
+
+module "arm-nvm" {
+    source = "./arm-nvm/class5"
+}
+
+module "arm-sp" {
+    source = "./arm-sp/class5"
+}
+
+module "sydney" {
+    source = "./sydney/class5"
+}
+
